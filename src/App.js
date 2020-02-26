8c52370a1061bc35ea6b3ee0c09c30ba235916d6
Maria-Teodora Pop <54820576+LadyShinka@users.noreply.github.com>
2020-02-26 19:32:07 +0000

    Fix tachyons problem

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Projects from './components/Projects/Projects';
 import Skills from './components/Skills/Skills';
 import Contact from './components/Contact/Contact';
 import './App.css';
+import './tachyons';
 
 function App() {
   return (
